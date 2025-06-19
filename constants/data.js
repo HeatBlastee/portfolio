6842ae03e38d1db4d482c19d79adbe3b26dd3218
HeatBlastee <mann42006@gmail.com>
2025-06-20 00:44:00 +0530

    all files

---
+++ constants/data.js
@@ -0,0 +1,73 @@
+const userData = {
+  githubUsername: "HeatBlastee",
+  name: "Mann Patel",
+  designation: "Full-Stack Developer",
+  avatarUrl: "/Heatblast.webp",
+  email: "23236@iiitu.ac.in",
+  phone: "+91 6352686028",
+  address: "Mehsana, Gujarat, India.",
+  projects: [
+    
+    {
+      title: "Widgetly",
+      link: "https://widgetly.vercel.app/",
+      imgUrl: "/widgetly.png",
+    },
+    {
+      title: "Quizz Me",
+      link: "https://quizzzme.vercel.app/",
+      imgUrl: "/quizzme.png",
+    },
+    {
+      title: "Slotsy",
+      link: "https://github.com/HeatBlastee/Slotsy",
+      imgUrl: "/slotsy.png",
+    }
+  ],
+  about: {
+    title: "I'm a software developer who loves building web apps that solve real problems.",
+description: [
+  `I started coding at a young age and quickly fell in love with creating things on the web. Over time, I’ve learned how to build full-stack applications that are fast, clean, and user-friendly.`,
+  `I enjoy working with modern tools like React, Next.js, Node.js, TypeScript, MongoDB, and PostgreSQL to bring ideas to life.`,
+  `Whether it's a personal project or a product for others, I’m always excited to build something that people find useful and enjoyable to use.`,
+],
+
+    currentProject: "Crackna",
+    currentProjectUrl: "http://crackna.com/",
+  },
+  experience: [
+    {
+      title: "Freelance Web Developer",
+      company: "Devanshu Verma",
+      year: "2025",
+      companyLink: "https://github.com/HeatBlastee",
+      desc: "Build a personal portfolio website for Devanshu Verma to highlight his profile and boost his site",
+    },
+    {
+      title: "Graduation - studing",
+      company: "IIIT Una.",
+      year: "Expected 2027",
+      companyLink: "https://github.com/HeatBlastee",
+      desc: "Major in Electronics and Communication Engineerig with a current CGPA of 8.51. Nobody asks this but it's okay.",
+    },
+    {
+      title: "High School",
+      company: "Vardhman Vidhyala, Mehsana",
+      year: "2023",
+      companyLink: "https://github.com/HeatBlastee",
+      desc: "Subjects being PCM - I survived!",
+    },
+  ],
+  resumeUrl:
+    "https://drive.google.com/file/d/18QYQxKLQyuIJLkJbDREdz2EDE7ugHcsK/view?usp=sharing",
+  socialLinks: {
+    instagram: "https://www.instagram.com/sleepyyy__ash/#",
+    twitter: "https://twitter.com/",
+    linkedin: "https://www.linkedin.com/in/mann-patel-321aa8289/",
+    github: "https://github.com/HeatBlastee",
+    facebook: "https://facebook.com/",
+    peerlist: "https://peerlist.io/",
+  },
+};
+
+export default userData;
